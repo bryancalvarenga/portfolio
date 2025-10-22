9a091cfda0fa77500bac6113a5b8ccf39a0aaafa
bryancalvarenga <bryancalvarenga@outlook.com.br>
2025-10-22 13:34:32 -0300

    feat: implement project modal with dynamic content and language support

---
+++ src/js/projects.js
@@ -0,0 +1,13 @@
+window.PROJECTS = {
+  sge: {
+    coverSrc: "./src/assets/img/sge-preview.webp",
+    tech: ["PHP 8.2", "MySQL", "Docker", "Bootstrap", "MVC", "PHPMailer", "JavaScript", "PDO", "HTML5", "CSS3", "AJAX", "Fetch API"],
+    links: {
+      demo: "#",          
+      repo: "https://github.com/rafaeldiasgarcia/sge"        
+    }
+  }
+
+  // Projetos futuros...
+
+};
